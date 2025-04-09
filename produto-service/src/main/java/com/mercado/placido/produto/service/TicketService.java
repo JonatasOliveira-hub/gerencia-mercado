b15c3e9ffc410bb6e3f90acb4d29360abf447201
Jonatas Oliveira Silva <JonatasOliveira@ibm.com>
2025-04-09 14:20:39 -0300

    Inclusão de config para produtos

--- produto-service/src/main/java/com/mercado/placido/produto/service/TicketService.java
+++ produto-service/src/main/java/com/mercado/placido/produto/service/TicketService.java
@@ -2,12 +2,13 @@ package com.mercado.placido.produto.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.mercado.placido.produto.domain.Ticket;
 
-@FeignClient
+@FeignClient(name = "ticket-server")
 public interface TicketService {
 
 	@GetMapping(value = "/ticket/produto/{produtoId}")
-	Ticket findByProdutoId(Integer produtoId);
+	Ticket findByProdutoId(@PathVariable Integer produtoId);
 }
